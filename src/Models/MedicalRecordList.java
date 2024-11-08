8ace102bc508d189e976fdd331da3a05fd8927d0
ZongZheJiang <jiangzongzhe374@gmail.com>
2024-11-09 02:00:37 +0800

    Initialised MedicalRecordList

--- src/Models/MedicalRecordList.java
+++ src/Models/MedicalRecordList.java
@@ -1,12 +1,35 @@
 package Models;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import Models.Record;
 
 public class MedicalRecordList {
 	ArrayList<MedicalRecord> medicalRecordList;
 
-	public MedicalRecordList() {
-		// TODO Auto-generated constructor stub
+	public MedicalRecordList() throws IOException {
+		try (BufferedReader br = new BufferedReader(new FileReader("../data/MedicalRecord_List.csv"))) {
+			String line;
+			br.readLine(); 
+			while ((line = br.readLine()) != null) {
+				MedicalRecord medicalRecord = null;
+				String[] row = line.split(",");
+				if (row.length >= 2) { 
+					String patientID = row[0].trim();
+					medicalRecord = new MedicalRecord(patientID);
+					String[] recordList = row[1].replaceAll("{}", "").split(");(");
+					for (String recordString : recordList) {
+						String[] recordParts = recordString.replaceAll("()", "").split(";");
+						this.medicalRecordList = new ArrayList<MedicalRecord>();
+						medicalRecord.recordList.add(new Record(recordParts[0], recordParts[1], recordParts[2]));
+					}
+				}
+				if (medicalRecord != null) {
+					medicalRecordList.add(medicalRecord);
+				}
+			}
+		}
 	}
-
 }
