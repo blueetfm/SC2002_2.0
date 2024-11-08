e3c171f82c409d5004ed41dfd655a58ec6b36003
ZongZheJiang <jiangzongzhe374@gmail.com>
2024-11-09 01:22:11 +0800

    Worked on Medical Record Functions

---
+++ src/Models/MedicalRecordList.java
@@ -0,0 +1,12 @@
+package Models;
+
+import java.util.ArrayList;
+
+public class MedicalRecordList {
+	ArrayList<MedicalRecord> medicalRecordList;
+
+	public MedicalRecordList() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
