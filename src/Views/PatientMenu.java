9293722b6b8e963cb9a57b50a47ac53b94d6c64b
blueetfm <tan.yx2004@gmail.com>
2024-11-20 19:42:40 +0800

    test

--- src/Views/PatientMenu.java
+++ src/Views/PatientMenu.java
@@ -18,12 +18,12 @@ public class PatientMenu implements Menu {
 			System.out.println("Perform the following methods:");
 			System.out.println("1: View Medical Record");
 			System.out.println("2: Update Personal Information");
-			System.out.println("3: View Available Appointment Slots");
-			System.out.println("4: Schedule an Appointment");
-			System.out.println("5: Reschedule an Appointment");
-			System.out.println("6: Cancel an Appointment");
-			System.out.println("7: View Scheduled Appointments");
-			System.out.println("8: View Past Appointment Outcome Records");
+			System.out.println("3: View Available Appointment Slots"); // patient class
+			System.out.println("4: Schedule an Appointment"); // patient class
+			System.out.println("5: Reschedule an Appointment"); // patient class
+			System.out.println("6: Cancel an Appointment"); // patient class
+			System.out.println("7: View Scheduled Appointments");  // patient class
+			System.out.println("8: View Past Appointment Outcome Records"); // patient class
 			System.out.println("9: Logout");
 			choice = sc.nextInt();
 			
